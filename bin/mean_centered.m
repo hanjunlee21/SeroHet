935f592eb493f6b955f16853467f7570b1d3b4c8
hanjunlee21 <67846757+hanjunlee21@users.noreply.github.com>
2021-05-15 18:58:04 -0400

    Add files via upload

---
+++ bin/mean_centered.m
@@ -0,0 +1,18 @@
+function output = mean_centered(matrix,groupID)
+
+% SeroHet software version 1.1
+% Copyright (c) 2021 Hanjun Lee, Seo Yihl Kim, Dong Wook Kim,
+% Young Soo Park, Jin-Hyeok Hwang, Sukki Cho, and Je-Yoel Cho.
+%
+% See the accompanying file LICENSE.txt for licensing details.
+
+output = zeros(size(matrix,1),size(matrix,2));
+matrix = log(1+matrix);
+m = mean(matrix(:,~strcmp(groupID,"Healthy Control")).').';
+sigma = std(matrix(:,~strcmp(groupID,"Healthy Control")).').';
+
+for x = 1:1:size(matrix,1)
+    for y = 1:1:size(matrix,2)
+        output(x,y) = (matrix(x,y)-m(x,1))/sigma(x,1);
+    end
+end
---
+++ bin/read_classification.m
@@ -0,0 +1,9 @@
+function class = read_classification(install_path,version)
+
+% SeroHet software version 1.1
+% Copyright (c) 2021 Hanjun Lee, Seo Yihl Kim, Dong Wook Kim,
+% Young Soo Park, Jin-Hyeok Hwang, Sukki Cho, and Je-Yoel Cho.
+%
+% See the accompanying file LICENSE.txt for licensing details.
+
+class = readmatrix([install_path '/library/SeroHet.v.' version(1) '.0.classification.txt'], 'FileType', 'text', 'Delimiter', '\t', 'OutputType', 'string');
